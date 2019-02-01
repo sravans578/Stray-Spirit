fdcd7afaa9cc14ca3b253c369933295ba642cfbf
AdityaGadhvi <adityagadhvi999@gmail.com>
2019-02-01 16:36:25 -0400

    Working on the slider

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { HomeComponent } from './home/home.component';
+import { PetProfilePageComponent } from './pet-profile-page/pet-profile-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'pet-profile-page',component: PetProfilePageComponent},
   { path: '**', component: NotfoundComponent }
 ];
 
---
+++ frontend/src/app/pet-profile-page/pet-profile-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-profile-page',
+  templateUrl: './pet-profile-page.component.html',
+  styleUrls: ['./pet-profile-page.component.scss']
+})
+export class PetProfilePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
