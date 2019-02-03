a075877c8aa903d442ee8bf1a97d9808167d95de
AdityaGadhvi <adityagadhvi999@gmail.com>
2019-02-03 10:42:14 -0400

    final commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,10 +13,7 @@ import { ReactiveFormsModule , FormsModule  } from '@angular/forms';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { HomeComponent } from './home/home.component';
 import { PetProfilePageComponent } from './pet-profile-page/pet-profile-page.component';
-<<<<<<< HEAD
 import { WhyAdoptCatComponent } from './why-adopt-cat/why-adopt-cat.component';
-=======
->>>>>>> b792a91fcf173f30d69c16e7a2a10e39c03ae0f5
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,12 +22,9 @@ import { WhyAdoptCatComponent } from './why-adopt-cat/why-adopt-cat.component';
     RegisterComponent,
     NotfoundComponent,
     HomeComponent,
-<<<<<<< HEAD
     PetProfilePageComponent,
-    WhyAdoptCatComponent
-=======
+    WhyAdoptCatComponent,
     PetProfilePageComponent
->>>>>>> b792a91fcf173f30d69c16e7a2a10e39c03ae0f5
   ],
   imports: [
     BrowserModule,
