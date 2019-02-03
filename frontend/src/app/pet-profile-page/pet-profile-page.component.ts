78e7d3b67ce2bf0887551f9c1223bca8af22f466
AdityaGadhvi <adityagadhvi999@gmail.com>
2019-02-02 22:46:04 -0400

    Completed the slider

--- frontend/src/app/pet-profile-page/pet-profile-page.component.ts
+++ frontend/src/app/pet-profile-page/pet-profile-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-pet-profile-page',
   templateUrl: './pet-profile-page.component.html',
-  styleUrls: ['./pet-profile-page.component.scss']
+  styleUrls: ['./pet-profile-page.component.scss'],
+  
 })
 export class PetProfilePageComponent implements OnInit {
 
