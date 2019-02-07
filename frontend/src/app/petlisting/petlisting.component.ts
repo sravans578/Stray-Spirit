e8cee5ff5446054008c241f8394761f15e258572
Dheeraj Varshney <dheerajvarshney01@gmail.com>
2019-02-07 16:04:12 -0400

    Petlisting ad product listing page completed

--- frontend/src/app/petlisting/petlisting.component.ts
+++ frontend/src/app/petlisting/petlisting.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./petlisting.component.scss']
 })
 export class PetlistingComponent implements OnInit {
-
+  pets: any = [];
   constructor() { }
 
   ngOnInit() {
-  }
+  
+  this.pets =
+  [
+    {
+      id:1, pettName:"pet1" , petPrice: 200, petImage : "dog1.jpg", petDesc: "petInfo"
+    },
+    {
+      id:2, pettName:"pet1" , petPrice: 200, petImage : "dog2.jpg", petDesc: "petInfo"
+    },
+    {
+      id:3, pettName:"pet1" , petPrice: 200, petImage : "dog3.jpg", petDesc: "petInfo"
+    },
+    {
+      id:4, pettName:"pet1" , petPrice: 200, petImage : "dog4.jpg", petDesc: "petInfo"
+    },
+    {
+      id:5, pettName:"pet1" , petPrice: 200, petImage : "dog5.jpg", petDesc: "petInfo"
+    },
+    {
+      id:6, pettName:"pet1" , petPrice: 200, petImage : "dog6.jpg", petDesc: "petInfo"
+    },
+    {
+      id:7, pettName:"pet1" , petPrice: 200, petImage : "cat1.jpg", petDesc: "petInfo"
+    },
+    {
+      id:8, pettName:"pet1" , petPrice: 200, petImage : "cat2.jpg", petDesc: "petInfo"
+    }
 
+  ];
 }
+}
+
--- frontend/src/app/productlisting/productlisting.component.ts
+++ frontend/src/app/productlisting/productlisting.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productlisting.component.scss']
 })
 export class ProductlistingComponent implements OnInit {
-
+  products: any = [];
   constructor() { }
 
   ngOnInit() {
+    this.products =
+    [
+      {
+        id:1, productName:"product1" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:2, productName:"product2" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:3, productName:"product3" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:4, productName:"product4" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:5, productName:"product5" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:6, productName:"product6" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:7, productName:"product7" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      },
+      {
+        id:8, productName:"product8" , productPrice: 200, productImage : "petproduct1.jpg", productDesc: "productInfo"
+      }
+
+    ];
   }
 
 }
