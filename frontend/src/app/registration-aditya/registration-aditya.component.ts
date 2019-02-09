46b743ebcd4812c4398769db290b7f571fbd2aa1
AdityaGadhvi <adityagadhvi999@gmail.com>
2019-02-09 18:00:03 -0400

    Done with my registration page including the navigation of it.

--- frontend/src/app/registration-aditya/registration-aditya.component.ts
+++ frontend/src/app/registration-aditya/registration-aditya.component.ts
@@ -27,62 +27,8 @@ export class RegistrationAdityaComponent implements OnInit {
     
   }
 
- onSubmit() {
-  
-  var password:string=this.userForm.get('passwordInput').value;
-  var cPassword:string=this.userForm.get('cPasswordInput').value;
-
-  if(password.match(cPassword)){
-    document.getElementById("errorDiv").setAttribute("style","display:none");
-  }
-  else{
-    document.getElementById("errorDiv").setAttribute("style","display:block");
-    document.getElementById("errorDiv").innerHTML="Passwords do not match";
-    
-  }
-
-   if(this.userForm.invalid){
-  document.getElementById("errorDiv").setAttribute("style","display:block");
-   }
-   else{
-    document.getElementById("errorDiv").setAttribute("style","display:none");
-   }
- }
-
  
 
 }
 
-// function confirmPassword(control:AbstractControl):{[key:string]:any} | null{
-//   var cPassword:string=control.value;
-//   var password:any=this.userForm.get('passwordInput').value;
-  
-//   if(cPassword.match(password.toString())){
-//     return null;
-//   }
-//   else{
-//     return {'confirmPassword':true};
-//   }
-//  }
 
- 
- 
- 
-// setPassword(control:AbstractControl):{[key:string]:any} | null{
-  
-//   this.mainpassword=control.value;
-
-//   return null;
-//  }
- 
-//  confirmPassword(control:AbstractControl):{[key:string]:any} | null{
-//   var cPassword:string=control.value;
-  
-  
-//   if(cPassword.match(this.mainpassword)){
-//     return null;
-//   }
-//   else{
-//     return {'confirmPassword':true};
-//   }
-//  }
