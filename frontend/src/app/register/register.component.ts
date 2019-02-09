976846697e1471199553c896bfe98e3deab52a48
MSI\Aadi Shah <shahaadesh5@gmail.com>
2019-02-09 17:46:10 -0400

    Validations done with redirections

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -3,20 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
   import {Title} from "@angular/platform-browser";
+  import {Router} from "@angular/router"
 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
 
+  namePattern: string = '^([a-zA-Z_\-]*)$';
+  emailPattern: string = '^[a-zA-Z0-9*_.-]+@[a-zA-Z]+[.][a-zA-Z]{2,3}$';
+  phonePattern: string = '^[\(][0-9]{3}[\)][\-][\(][0-9]{3}[\)][\-][\(][0-9]{4}[\)]$|[0-9]{10}|[0-9]{3}-[0-9]{3}-[0-9]{4}';
+  
+  submitted: boolean = false;
+  
+  
   registerForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    number: new FormControl('', Validators.required),
+    firstName: new FormControl('', [Validators.required,Validators.pattern(this.namePattern)]),
+    lastName: new FormControl('', [Validators.required,Validators.pattern(this.namePattern)]),
+    email: new FormControl('', [Validators.required, Validators.email,Validators.pattern(this.emailPattern)]),
+    number: new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     repassword: new FormControl('', [Validators.required, Validators.minLength(8)])
   })
@@ -26,17 +34,26 @@ export class RegisterComponent implements OnInit {
   // }
 
   orgRegisterForm = new FormGroup({
-    orgName: new FormControl('', Validators.required),
-    orgEmail: new FormControl('', [Validators.required, Validators.email]),
-    orgPhone: new FormControl('', Validators.required),
+    orgName: new FormControl('', [Validators.required,Validators.pattern(this.namePattern)]),
+    orgEmail: new FormControl('', [Validators.required, Validators.email,Validators.pattern(this.emailPattern)]),
+    orgPhone: new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
     orgRegNo: new FormControl('', Validators.required),
     orgPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
     orgRePassword: new FormControl('', [Validators.required, Validators.minLength(8)])
   })
-  constructor(private titleService:Title) { 
+  constructor(private titleService:Title,private router: Router) { 
     this.titleService.setTitle("Register on StraySpirit");
    }
 
   ngOnInit() {
+  }
+  userRegisterSubmit(){
+    this.submitted=true;
+    setTimeout(()=>{  
+      this.submitted = false;
+ }, 3000);
+ setTimeout(()=>{  
+  this.router.navigate(['/login']);
+}, 3000);
   }
 }
