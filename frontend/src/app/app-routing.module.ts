af6e03db4d5a192001d07df9d7c1c77e85b850e5
Dheeraj Varshney <dheerajvarshney01@gmail.com>
2019-02-09 17:05:26 -0400

    initial commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,6 +14,8 @@ import { ProfileInventoryComponent } from './profile-inventory/profile-inventory
 import { ProfileNavComponent } from './profile-nav/profile-nav.component';
 import {ProductDetailPageComponent} from './product-detail-page/product-detail-page.component';
 import { WhyadoptdogComponent } from './whyadoptdog/whyadoptdog.component';
+import { LoginDheerajComponent } from './login-dheeraj/login-dheeraj.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,6 +26,8 @@ const routes: Routes = [
   { path: 'pet-profile-page',component: PetProfilePageComponent},
   { path:'why-adopt-cat' ,component:WhyAdoptCatComponent},
   { path: 'why-adopt-dog',component:WhyadoptdogComponent},
+  { path: 'login-Dheeraj',component:LoginDheerajComponent},
+
   // Implemented nested routes https://angular-2-training-book.rangle.io/handout/routing/child_routes.html
   { path:'profile', 
     component:ProfileNavComponent,
