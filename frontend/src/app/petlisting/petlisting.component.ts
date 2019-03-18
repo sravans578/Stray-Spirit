d767d7fb352830b9f85504b277a887583e97ba1d
MSI\Aadi Shah <shahaadesh5@gmail.com>
2019-03-18 18:34:41 -0300

    Location filter with error messages and not found error

--- frontend/src/app/petlisting/petlisting.component.ts
+++ frontend/src/app/petlisting/petlisting.component.ts
@@ -5,6 +5,8 @@ import { LocationService } from '../location.service';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PetmanagementService } from '../petmanagement.service';
+import {Title} from "@angular/platform-browser";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-petlisting',
@@ -15,7 +17,8 @@ export class PetlistingComponent implements OnInit {
   pets: any = [];
 
   api_location: any;
-  pet_newData: any;
+  pet_newData: [];
+  all_Pets: any;
   current_location: any;
   comma: string =', ';
   label_value: any;
@@ -34,63 +37,69 @@ export class PetlistingComponent implements OnInit {
     testSearch: new FormControl('') 
   });
 
-  constructor(private loc: LocationService, private petService: PetmanagementService) { }
+  constructor(
+    private loc: LocationService, 
+    private petService: PetmanagementService,
+    private toastr: ToastrService, 
+    private titleService:Title) { 
+      this.titleService.setTitle("Pets Availble for Adoption near you - StraySpirit");
+  }
 
   ngOnInit() {
   
     this.loc.getCurrentLocation().subscribe(currentData =>{
-      console.log(currentData);
+      //console.log(currentData);
       this.current_location=currentData;
       this.currentCity=currentData.city;
       this.currentState=currentData.region_code;
       this.currentCountry=currentData.country_name;
-      console.log(this.currentCity);
+      //console.log(this.currentCity);
       this.inputLocation = this.currentCity+this.comma+this.currentState+this.comma+this.currentCountry;
       this.searchLocation.patchValue(this.inputLocation);
     })
 
     this.petService.getPets().subscribe(petData =>{
-      console.log(petData);
-      this.pet_newData=petData;
+      //console.log(petData);
+      this.all_Pets=petData
+      this.pet_newData=this.all_Pets.filter(filterPets =>{
+        return filterPets.petLocation.petCity.includes(this.currentCity) && filterPets.petLocation.petState.includes(this.currentState) && filterPets.petLocation.petCountry.includes(this.currentCountry);
+      });
+      console.log(this.all_Pets);
+      console.log(this.pet_newData);
     })
   
 }
 filterPet(searchTerm){
-  console.log(searchTerm);
+  //console.log(searchTerm);
   this.searchLocationTerm = searchTerm.split(',');
   this.searchLocationTerm[0] =this.searchLocationTerm[0].replace(/\s/g, "");
   this.searchLocationTerm[1] =this.searchLocationTerm[1].replace(/\s/g, "");
   this.searchLocationTerm[2] =this.searchLocationTerm[2].replace(/\s/g, "");
-  console.log(this.searchLocationTerm);
-  // var i;
-  // for(i=0;i<this.pet_newData.length;i++){
-  //   if(this.pet_newData[i].petLocation.petCity===searchLocationTerm[0]){
-  //     this.filteredLocation=this.pet_newData;
-  //     console.log("City Match found!");
-  //     break;
-  //   }
-  //   else{
-  //     console.log("No matching city!");
-  //   }
-  // }
+  //console.log(this.searchLocationTerm);
 }
 onSearch(){
-  this.pet_newData = this.pet_newData.filter(filterPets =>{
-    return filterPets.petLocation.petCity.includes(this.searchLocationTerm[0]) && filterPets.petLocation.petState.includes(this.searchLocationTerm[1]) && filterPets.petLocation.petCountry.includes(this.searchLocationTerm[2]);
-  })
-  console.log("Pet data after filter",this.pet_newData);
+  if(!this.searchLocationTerm){
+    console.log("No search term!");
+    this.toastr.error('Select different Location or pets are already listed for current location', 'Error!', {
+      timeOut: 5500,
+      closeButton: true,
+      progressBar: true
+    });
+  }
+  else{
+    this.pet_newData = this.all_Pets.filter(filterPets =>{
+      return filterPets.petLocation.petCity.includes(this.searchLocationTerm[0]) && filterPets.petLocation.petState.includes(this.searchLocationTerm[1]) && filterPets.petLocation.petCountry.includes(this.searchLocationTerm[2]);
+    })
+  }
+  //console.log("Pet data after filter",this.pet_newData);
 }
 onKeydown(event:any) {
     this.city=event.target.value;
     if(this.city.length>2){
     this.loc.getLocation(this.city).subscribe(data =>{
-      console.log(data);
+      //console.log(data);
       this.api_location = data;
-      console.log(this.api_location);
-      // for (this.i = 0; this.i < 5; this.i++) {
-      //   this.city=this.api_location.suggestions[this.i].label;
-      //   console.log(this.city);
-      // }
+      //console.log(this.api_location);
     })
   }
 
