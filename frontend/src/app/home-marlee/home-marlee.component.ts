285c82ba55d9f500a87a3fb9bae71eefd5f32d9f
Marlee Donnelly <marlee.donnelly@dashhudson.com>
2019-05-27 20:24:44 -0300

    Added featured pet row

--- frontend/src/app/home-marlee/home-marlee.component.ts
+++ frontend/src/app/home-marlee/home-marlee.component.ts
@@ -9,6 +9,7 @@ export class HomeMarleeComponent implements OnInit {
 
   //TODO: replace with real event objects
   sampleEvents = ['Event 1', 'Event 2', 'Event 3', 'Event 4', 'Event 5'];
+  samplePets = ['Bran', 'Arya', 'Jon', 'Sansa', 'Rickon', 'Robb'];
 
   constructor() { }
 
