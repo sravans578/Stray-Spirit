847c36a7b8a2ac3eae1482d3987cf690ee4091f7
Marlee Donnelly <marlee.donnelly@dashhudson.com>
2019-05-29 21:16:28 -0300

    Added form validation to login page

--- frontend/src/app/login-marlee/login-marlee.component.ts
+++ frontend/src/app/login-marlee/login-marlee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-marlee',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginMarleeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+  loginForm: FormGroup;
+  submitted = false;
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password:['', Validators.required ]
+    })
+  }
+
+  get validationChecks(){
+    return this.loginForm.controls;
+  }
+
+  attemptLogin(){
+    this.submitted = true;
+    if(this.loginForm.invalid){
+      console.log('Login failed.');
+      return
+    }
+    else{
+      console.log('Login successful!');
+    }
   }
 
 }
