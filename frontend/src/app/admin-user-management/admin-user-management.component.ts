fd074264111d950ac6924d7892d7c63a638f6aac
Marlee Donnelly <marlee.donnelly@dashhudson.com>
2019-05-31 16:21:56 -0300

    Added brains to the manage users page
    - Methods for switching between sample data sets
    - Confirmation popup for delete action
    - Enable/disable admin privilege button change

--- frontend/src/app/admin-user-management/admin-user-management.component.ts
+++ frontend/src/app/admin-user-management/admin-user-management.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminUserManagementComponent implements OnInit {
 
-  allUsers = [{name: 'Remus Lupin', isAdmin: true }, {name: 'Minerva McGonagall', isAdmin: true}, {name: 'Aurora Sinistra', isAdmin: false},
-    {name: 'Sybil Trelawney', isAdmin: true}, {name: 'Severus Snape', isAdmin:false}, {name: 'Horace Slughorn', isAdmin: false},
-    {name: 'Charity Burbidge', isAdmin: false}, {name: 'Rubeus Hagrid',isAdmin:true}, {name: 'Pomona Sprout', isAdmin:true},
-    {name: 'Filius Flitwick', isAdmin:true}, {name: 'Albus Dumbledore',isAdmin:false}, {name: 'Argus Filch', isAdmin: false},
-    {name: 'Dolores Umbridge', isAdmin: false}, {name: 'Alecta Carrow', isAdmin: false}, {name:'Gilderoy Lockhart', isAdmin: false},
-    {name: 'Alastor Moody', isAdmin:true}, {name: 'Rolanda Hooch', isAdmin: false}, {name: 'Irma Pince', isAdmin:true},
-    {name: 'Quirinus Quirrell', isAdmin: false}];
+  allUsers = [{id: 0, name: 'Remus Lupin', isAdmin: true }, {id: 1, name: 'Minerva McGonagall', isAdmin: true}, {id: 2, name: 'Aurora Sinistra', isAdmin: false},
+    {id: 3, name: 'Sybil Trelawney', isAdmin: true}, {id: 4, name: 'Severus Snape', isAdmin:false}, {id: 5, name: 'Horace Slughorn', isAdmin: false},
+    {id: 6, name: 'Charity Burbidge', isAdmin: false}, {id: 7, name: 'Rubeus Hagrid',isAdmin:true}, {id: 8, name: 'Pomona Sprout', isAdmin:true},
+    {id: 9, name: 'Filius Flitwick', isAdmin:true}, {id: 10, name: 'Albus Dumbledore',isAdmin:false}, {id: 11, name: 'Argus Filch', isAdmin: false},
+    {id: 12, name: 'Dolores Umbridge', isAdmin: false}, {id: 13, name: 'Alecta Carrow', isAdmin: false}, {id: 14, name:'Gilderoy Lockhart', isAdmin: false},
+    {id: 15, name: 'Alastor Moody', isAdmin:true}, {id: 16, name: 'Irma Pince', isAdmin:true}, {id: 17, name: 'Quirinus Quirrell', isAdmin: false}];
+
+  organizations = [{id: 18, name: 'Dalhousie', isAdmin: false}, {id: 19, name: 'StraySpirit', isAdmin:true}, {id: 20, name: 'Hogwarts', isAdmin:false},
+    {id: 21, name: 'Fake Inc.', isAdmin: true}, {id: 22, name: 'Totally a Real Company', isAdmin:false}];
+
+  currentList = this.allUsers;
+  showAdminTable = false;
+  startingIndex = 0;
 
   constructor() { }
 
@@ -24,6 +30,30 @@ export class AdminUserManagementComponent implements OnInit {
     alert("Are you sure you want to delete this user?");
   }
   showUsers(){
-
+    this.currentList = this.allUsers;
+    this.showAdminTable = false;
+  }
+  showOrganizations(){
+    this.currentList = this.organizations;
+    this.showAdminTable = false;
+  }
+  showAdmins(){
+    var admins = [];
+    for (let user of this.allUsers){
+      if(user.isAdmin) {
+        admins.push(user);
+      }
+    }
+    this.currentList = admins;
+    this.showAdminTable = true;
+  }
+  toggleAdmin(id){
+    //TODO: implement more efficiently with queries once there's a real database to search
+    for(let user of this.currentList){
+      if(user.id == id){
+        user.isAdmin = !user.isAdmin;
+        break;
+      }
+    }
   }
 }
